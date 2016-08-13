2b567dfcee78275636869ae4c40dd5f554e1e6e6
ahmedzein <elze0010@algonquinlive.com>
2016-08-13 17:48:24 -0400

    feat(js): image flip
    change the image when the user hovers their mouse over the image

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.app-icon').mouseover(function(){
+  $('.app-icon').attr("src","images/app-icon-yellow.svg");
+});
+
+$('.app-icon').mouseout(function(){
+  $('.app-icon').attr("src","images/app-icon-red.svg");
+});
