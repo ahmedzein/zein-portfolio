12f450ed6b361cb662e0421f33bd2b24f08a8f95
ahmedzein <elze0010@algonquinlive.com>
2016-08-14 13:57:13 -0400

    docs(js): remove js
    remove js from the website

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$('.app-icon').mouseover(function(){
-  $('.app-icon').attr("src","images/app-icon-yellow.svg");
-});
-
-$('.app-icon').mouseout(function(){
-  $('.app-icon').attr("src","images/app-icon-red.svg");
-});
